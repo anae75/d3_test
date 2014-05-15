9272c0a72c7cb55e998c5854bb92f37c9084ddb3
anae75 <ana.devel@gmail.com>
2014-05-15 10:53:05 -0400

    histogram

--- d3_toy.js
+++ d3_toy.js
@@ -166,7 +166,8 @@ ColorSpec.prototype.picker = function()
 
 //------------------------------------------------------------
 
-function pie_chart(data, path, color) {
+function pie_chart(data, path, color) 
+{
   var width = 300;
   var height = 300;
   var radius = 100;
@@ -209,6 +210,65 @@ function pie_chart(data, path, color) {
 }
 
 //------------------------------------------------------------
+function histogram(dataset, attr, path, color)
+{
+  var raw_values = jQuery.map(dataset.data_rows, function(item,i) {return item[attr];});
+
+  var formatCount = d3.format(",.0f");
+
+  var margin = {top: 10, right: 30, bottom: 30, left: 30};
+  var width = 550 - margin.left - margin.right;
+  var height = 300 - margin.top - margin.bottom;
+
+  var scale_x = d3.scale.linear()
+                .domain([0,d3.max(raw_values)])
+                .range([0,width]);
+
+  // array of groups of binned values
+  var data = d3.layout.histogram()
+      .bins(scale_x.ticks(20))            // returns a function based on an array of tick values
+      (raw_values);
+
+  var scale_y = d3.scale.linear()
+                .domain([0, d3.max(data, function(d) { return d.y; })])
+                .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(scale_x)
+      .orient("bottom");
+
+  var svg = d3.select(path + " svg.chart")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var bar = svg.selectAll(".bar")
+      .data(data)
+      .enter().append("g")
+      .attr("class", "bar")
+      .attr("transform", function(d) { return "translate(" + scale_x(d.x) + "," + scale_y(d.y) + ")"; });
+
+  bar.append("rect")
+      .attr("x", 1)
+      .attr("width", scale_x(data[0].dx) - 1)
+      .attr("height", function(d) { return height - scale_y(d.y); });
+
+  bar.append("text")
+      .attr("dy", ".75em")
+      .attr("y", 6)
+      .attr("x", scale_x(data[0].dx) / 2)
+      .attr("text-anchor", "middle")
+      .text(function(d) { return formatCount(d.y); });
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+}
+
+//------------------------------------------------------------
 //------------------------------------------------------------
 function init()
 {
@@ -230,6 +290,8 @@ function init()
                         .set("Joint", "yellow")
                         .set("Sole", "green");
   pie_chart(count_pct(dataset, "authority_label"), "#authority_label ", colorspec.picker());
+
+  histogram(dataset, "opportunity", "#opportunity", null);
 }
 
 jQuery(document).ready( function() { init(); } );
